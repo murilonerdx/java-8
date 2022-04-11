8f946c2f3c391148f5cf021a1c2477c9f6b92b25
murilonerdx <mu-silva@outlook.com>
2022-04-11 10:19:27 -0300

    Stream

---
+++ src/com/murilonerdx/java8/Curso.java
@@ -0,0 +1,62 @@
+package com.murilonerdx.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+class Curso {
+    private String nome;
+    private int alunos;
+
+    public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "nome='" + nome + '\'' +
+                ", alunos=" + alunos +
+                '}';
+    }
+
+    public static class ExemploCursos{
+        public static void main(String[] args){
+            List<Curso> cursos = new ArrayList<Curso>();
+            cursos.add(new Curso("Python", 45));
+            cursos.add(new Curso("JavaScript", 150));
+            cursos.add(new Curso("Java 8", 113));
+            cursos.add(new Curso("C", 55));
+
+            cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+
+            cursos.forEach(System.out::print);
+            cursos.forEach(Curso::getNome);
+
+            cursos.stream()
+                    .filter(c -> c.getAlunos() >= 100)
+                    .map(Curso::getAlunos)
+                    .forEach(System.out::println);
+
+            int sum = cursos.stream()
+                    .filter(c -> c.getAlunos() >= 100)
+                    .mapToInt(Curso::getAlunos)
+                    .sum();
+
+            System.out.println(sum);
+
+        }
+    }
+}
+
+
